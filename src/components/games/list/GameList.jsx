9f951f8125340e842e5a5272f1dd84b40b5a2d0a
Ahmad salman <ahmadg.salman@hotmail.com>
2023-02-05 12:21:12 +0200

    Add mui pagination

--- src/components/games/list/GameList.jsx
+++ src/components/games/list/GameList.jsx
@@ -1,10 +1,12 @@
 import React, { useState, lazy, Suspense } from "react";
-import ReactPaginate from "react-paginate";
+// import ReactPaginate from "react-paginate";
 import useLocalStorage from "use-local-storage";
 import { GameItem } from "../gameItem/GameItem";
 import style from "./GameList.module.css";
 import { listType } from "./Types";
 import { tabs } from "./Types";
+import { Pagination } from "@mui/material";
+
 const GameItemHorizontal = lazy(() =>
   import("../gameItemHorizontal/GameItemHorizontal")
 );
@@ -43,8 +45,12 @@ export const GameDetailsList = ({
     }
   }
 
-  const handlePageChange = ({ selected }) => {
-    setCurrentPage(selected + 1);
+  // const handlePageChange = ({ selected }) => {
+  //   setCurrentPage(selected + 1);
+  // };
+
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
   };
 
   return (
@@ -82,24 +88,32 @@ export const GameDetailsList = ({
       </div>
 
       {gameList !== null && isShowPagination && (
-        <ReactPaginate
-          previousLabel="Previous"
-          nextLabel="Next"
-          pageClassName="page-item"
-          pageLinkClassName="page-link"
-          previousClassName="page-item"
-          previousLinkClassName="page-link"
-          nextClassName="page-item"
-          nextLinkClassName="page-link"
-          breakLabel="..."
-          breakClassName="page-item"
-          breakLinkClassName="page-link"
-          pageCount={pageCount}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={5}
-          onPageChange={handlePageChange}
-          containerClassName="pagination"
-          activeClassName="active"
+        //  (
+        //   <ReactPaginate
+        //     previousLabel="Previous"
+        //     nextLabel="Next"
+        //     pageClassName="page-item"
+        //     pageLinkClassName="page-link"
+        //     previousClassName="page-item"
+        //     previousLinkClassName="page-link"
+        //     nextClassName="page-item"
+        //     nextLinkClassName="page-link"
+        //     breakLabel="..."
+        //     breakClassName="page-item"
+        //     breakLinkClassName="page-link"
+        //     pageCount={pageCount}
+        //     marginPagesDisplayed={2}
+        //     pageRangeDisplayed={5}
+        //     onPageChange={handlePageChange}
+        //     containerClassName="pagination"
+        //     activeClassName="active"
+        //   />
+        // )
+
+        <Pagination
+          count={pageCount}
+          page={currentPage}
+          onChange={handlePageChange}
         />
       )}
     </div>
